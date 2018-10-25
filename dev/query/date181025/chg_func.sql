d153ccd9142f59388897d805946f36f695eef189
YOUNG OH <younggeun0@mme.dongguk.edu.com>
2018-10-25 14:00:34 +0900

    181025 교육내용 추가

--- dev/query/date181025/chg_func.sql
+++ dev/query/date181025/chg_func.sql
@@ -19,9 +19,36 @@ FROM emp;
 SELECT TO_CHAR(sal, '9,999')+100
 FROM emp;
 
-SELECT TO_CHAR(SYSDATE, 'y'),TO_CHAR(SYSDATE, 'yy'
-	   TO_CHAR(SYSDATE, 'yyy')
+-- 날짜를 문자열로 변환 : TO_CHAR(날짜, pattern)
+SELECT TO_CHAR(SYSDATE, 'y'), TO_CHAR(SYSDATE, 'yy'),
+	   TO_CHAR(SYSDATE, 'yyy'), TO_CHAR(SYSDATE, 'yyyy'),
+	   TO_CHAR(SYSDATE, 'yyyyy')
 FROM DUAL;
+
+-- 월과 일은 mm, dd처럼 두개 꼭 두개 이상 써야 한다
+-- 내가 보고싶은 형태로 패턴을 만듦
+SELECT TO_CHAR(SYSDATE, 'yyyy-mm-dd'), TO_CHAR(SYSDATE, 'mm-dd-yyyy')
+FROM DUAL;
+
+SELECT TO_CHAR(SYSDATE, 'yyyy-mm-dd am hh(hh24)')
+FROM DUAL;
+
+-- 날짜형이 시간형태로 입력이 되면 시간정보는 없음, 날짜 정보만 있음
+-- SYSDATE만 시간정보 갖는다
+SELECT TO_CHAR(SYSDATE, 'yyyy-mm-dd am hh(hh24):mi:ss')
+FROM DUAL;
+
+SELECT TO_CHAR(SYSDATE, 'yyyy-mm-dd am hh(hh24):mi:ss day dy q')
+FROM DUAL;
+
+
+-- perttern이 특수문자가 아닌 문자열 사용할때에는 "로 묶는다
+SELECT TO_CHAR(SYSDATE, 'yyyy"년" mm"월" dd"일"')
+FROM DUAL;
+
+
+-- perttern을 너무 길게 사용하면 Error 발생!
+-- 어지간히 써선 에러 안남
 
 
 
