038d2d522f00299af2f7aeff1dd2f2049caf6048
YOUNG OH <younggeun0@mme.dongguk.edu.com>
2018-11-09 08:48:09 +0900

    숙제풀이

--- dev/query/date181108_pl/homework1.sql
+++ dev/query/date181108_pl/homework1.sql
@@ -6,54 +6,37 @@ user_constraints, user_tab_cols, user_tab_columns
 을 조회하는 쿼리문을 작성 .
 
 2. 위의 작업을 수행하는 Procedure를 생성.
-
-
 */
-SELECT * FROM user_constraints;  
+SELECT * FROM user_constraints;
 -- CONSTRAINT_NAME
 -- 제약사항 테이블엔 컬럼정보가 존재X
-SELECT * FROM user_tab_cols;     
+SELECT * FROM user_tab_cols;
 -- TABLE_NAME, COLUMN_NAME, DATA_TYPE
+
+SELECT * FROM user_cons_columns;
 
 -- CONSTRAINT_NAME, COLUMN_NAME, DATA_TYPE 을 출력
 
+--
 
+SELECT  utc.TABLE_NAME, uc.CONSTRAINT_NAME, utc.COLUMN_NAME, utc.DATA_TYPE
+FROM   user_tab_cols utc, user_constraints uc
+WHERE  (uc.TABLE_NAME = utc.TABLE_NAME)
+ AND utc.TABLE_NAME='EMP';
 
+-- 조인, 스칼라서브쿼리 사용
+-- 일단 필요컬럼 있는 조인은 했는데.. 이제 어떻게 줄이지
+SELECT  *
+FROM   user_tab_cols utc, user_constraints uc, user_cons_columns ucc
+WHERE  (uc.TABLE_NAME = utc.TABLE_NAME AND uc.COLUMN_NAME = ucc.COLUMN_NAME)
+ AND ucc.TABLE_NAME='EMP';
 
-
-
-
---SELECT uc.CONSTRAINT_NAME, utc.COLUMN_NAME, utc.DATA_TYPE
-SELECT *
+SELECT  *
 FROM   (SELECT TABLE_NAME
 				FROM user_tab_cols
 				GROUP BY TABLE_NAME) utc, user_constraints uc
 WHERE  (uc.TABLE_NAME = utc.TABLE_NAME)
- AND uc.CONSTRAINT_NAME LIKE '%'
  AND utc.TABLE_NAME='EMP';
 
-SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE
-FROM user_tab_cols
-UNION
-SELECT TABLE_NAME, CONSTRAINT_NAME
-FROM user_constraints;
 
--- 유니온하고 그 결과에서 값을 뽑아내기                               
--- user_tab_cols
--- TABLE_NAME(VARCHAR2(30), COLUMN_NAME(VARCHAR2(30), DATA_TYPE(VARCHAR2(106)
-                
--- user_constraints
--- TABLE_NAME(VARCHAR2(30), CONSTRAINT
-/*
 
-SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE
-FROM(
-	SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE
-	FROM user_tab_cols
-	UNION
-	SELECT TABLE_NAME, R_OWNER, R_CONSTRAINT_NAME
-	FROM user_constraints
-)
-GROUP BY TABLE_NAME;
-
-*/
