0c3596a939f6ec797cc73d5c93e63ae3410f4356
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2018-12-05 17:57:46 +0900

    숙제 수정

--- dev/workspace/sistJavaStudy/src/date181205/Lotto.java
+++ dev/workspace/sistJavaStudy/src/date181205/Lotto.java
@@ -22,12 +22,13 @@ public class Lotto {
 		}
 	}
 	
+	// 공 섞기 영근ver.
+	// 이미 있는 공들의 순서만 바꾼 뒤 6개 선택하는 방법
 	public int[] pickBall(int[] ball) {
 		int[] picked = new int[6];
 		int idx = 0;
 		int tmp = 0;
 		
-		// 공 섞기
 		for(int i=0; i<1000; i++) {
 			idx = (int)(Math.random()*45);
 			tmp = ball[idx];
@@ -40,6 +41,40 @@ public class Lotto {
 			picked[i] = ball[i];
 		}
 		
+		return picked;
+	}
+	
+	// 재찬이 ver. 문제는 같을 때 랜덤한 값을 다시 입력하나
+	// 동일한 공이 또 나올 가능성이 존재
+	public int[] pickBall2(int[] ball) {
+		int[] picked = new int[6];
+		
+		for(int i=0; i<picked.length; i++) {
+			picked[i] = ball[(int)(Math.random()*45)];
+			for(int j=0; j<i; j++) {
+				if(picked[i] == picked[j]) { 
+					picked[i] = ball[(int)(Math.random()*45)];
+				}
+			}
+		}
+		return picked;
+	}
+	
+	// 영민 ver.
+	// 공을 하나 택하고 이전부터 뽑은공까지 비교 후 
+	// 같은 값의 공이 존재시 다시 뽑기
+	public int[] pickBall3(int[] ball) {
+		int[] picked = new int[6];
+		
+		for(int i=0; i<picked.length; i++) {
+			picked[i] = ball[(int)(Math.random()*45)];
+			for(int j=0; j<i; j++) {
+				if(picked[i] == picked[j]) {
+					i--;
+					break;
+				}
+			}
+		}
 		return picked;
 	}
 	
@@ -61,7 +96,6 @@ public class Lotto {
 	
 	public void printBall(int[] sortedBall) {
 		
-		System.out.println("------------선택된 6가지 공----------");
 		for(int i=0; i<sortedBall.length; i++) {
 			System.out.print(sortedBall[i]+" ");
 		}
--- dev/workspace/sistJavaStudy/src/date181205/UseLotto.java
+++ dev/workspace/sistJavaStudy/src/date181205/UseLotto.java
@@ -6,7 +6,17 @@ public class UseLotto {
 
 		Lotto lotto = new Lotto();
 
+		// 영근 ver.
+		System.out.println("영근 ver.");
 		lotto.printBall(lotto.sortBall(lotto.pickBall(lotto.getBall())));
 
+		// 재찬 ver. 
+		System.out.println("재찬 ver.");
+		lotto.printBall(lotto.sortBall(lotto.pickBall2(lotto.getBall())));
+
+		// 영민 ver.
+		System.out.println("영민 ver.");
+		lotto.printBall(lotto.sortBall(lotto.pickBall3(lotto.getBall())));
+		
 	}
 }
