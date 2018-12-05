69a9542d4784ce84e2c946cc14d4b1a84d06c111
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2018-12-05 19:33:25 +0900

    숙제수정

--- dev/workspace/sistJavaStudy/src/date181205/StringTokenizerHW.java
+++ dev/workspace/sistJavaStudy/src/date181205/StringTokenizerHW.java
@@ -8,17 +8,39 @@ package date181205;
 import java.util.StringTokenizer;
 
 public class StringTokenizerHW {
+	
+	public String[] getName(String str) {
+		
+		StringTokenizer stk = new StringTokenizer(str, ",.~!");
+		String[] name = new String[stk.countTokens()];
+		
+		int idx = 0;
+		while(stk.hasMoreTokens()) {
+			name[idx] = stk.nextToken();
+			idx++;
+		}
+		
+		return name;
+	}
 
 	public static void main(String[] args) {
 		
 		String name = "이재찬,이재현.공선의,정택성~이재현!최지우.김희철,이재찬,이재찬,정택성.공선의";
 		
-		StringTokenizer stk = new StringTokenizer(name, ",.~!");
+		StringTokenizerHW stkHW = new StringTokenizerHW();
+		
+		String[] names = stkHW.getName(name);
+		
+		for(int i=0; i<names.length; i++) {
+			System.out.print(names[i]+" ");
+		}
+		
+		/*StringTokenizer stk = new StringTokenizer(name, ",.~!");
 		
 		System.out.println("출력 : ");
 		while(stk.hasMoreTokens()) {
 			System.out.print(stk.nextToken()+" ");
 		}
-		System.out.println();
+		System.out.println();*/
 	}
 }
