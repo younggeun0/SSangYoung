bdf1216d3c4c2fea151878e3faa5c0230e2dc622
Young Oh <34850791+younggeun0@users.noreply.github.com>
2018-12-08 20:48:18 +0900

    181208 자습

---
+++ dev/workspace/sistJavaStudy/src/date181208/DayCheck.java
@@ -0,0 +1,59 @@
+package date181208;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DayCheck {
+
+	public Map<Integer,String> getDay(String[] day) {
+		int tmp = 0;
+
+		String[] dayOfWeek = { "일","월","화","수","목","금","토" };
+		Set<Integer> set = new HashSet<>();
+		Map<Integer, String> map = new HashMap<>();
+		Calendar c = Calendar.getInstance();
+		
+		for(int i=0; i<day.length; i++) {
+			tmp = Integer.parseInt(day[i]);
+			if (tmp > 0 && tmp < 32) {
+				set.add(tmp);
+			}
+		}
+		
+		Integer[] arrDay = new Integer[set.size()];
+		set.toArray(arrDay);
+		
+		for(int i=0; i<arrDay.length; i++) {
+			c.set(Calendar.DAY_OF_MONTH, arrDay[i]);
+			map.put(arrDay[i], dayOfWeek[c.get(Calendar.DAY_OF_WEEK)-1]);
+		}
+		
+		return map;
+	}
+	
+	public void printDay(Map<Integer,String> map) {
+		
+		int tmp = 0;
+
+		Set<Integer> key = map.keySet();
+		Integer[] arrKey = new Integer[map.size()];
+		key.toArray(arrKey);
+		
+		for(int i=0; i<arrKey.length-1; i++) {
+			for(int j=0; j<arrKey.length-1-i; j++) {
+				if(arrKey[j] > arrKey[j+1]) {
+					tmp = arrKey[j];
+					arrKey[j] = arrKey[j+1];
+					arrKey[j+1] = tmp;
+				}
+			}
+		}
+		
+		for(int i=0; i<map.size(); i++) {
+			System.out.println(arrKey[i]+" "+map.get(arrKey[i]));
+		}
+	}
+}
---
+++ dev/workspace/sistJavaStudy/src/date181208/RunDayCheck.java
@@ -0,0 +1,23 @@
+package date181208;
+/*
+숙제 1.
+main method의 arguments로 날짜를 여러개 입력 받아 입력받은 날짜의
+요일을 저장하고 출력하는 method 를 작성하세요.
+
+예) java Test 4 12 30 32 35 15 4   
+
+출력) 4 화
+     12 수
+     15 토
+     30 일  
+  
+*/
+public class RunDayCheck {
+
+	public static void main(String[] args) {
+		
+		DayCheck dc = new DayCheck();
+		
+		dc.printDay(dc.getDay(args));
+	}
+}
