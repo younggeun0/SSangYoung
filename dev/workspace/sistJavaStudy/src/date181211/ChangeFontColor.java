cfb650092ca8c9015cd1d8b005ed217243598b38
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2018-12-11 17:54:53 +0900

    숙제 배열사용으로 수정

--- dev/workspace/sistJavaStudy/src/date181211/ChangeFontColor.java
+++ dev/workspace/sistJavaStudy/src/date181211/ChangeFontColor.java
@@ -20,13 +20,11 @@ public class ChangeFontColor extends Frame implements ItemListener{
 		lbl.setBounds(30, 50, 40, 20);
 		taEx = new TextArea("오늘은 화요일");
 		taEx.setBounds(30, 80, 150, 100);
+		String[] arrColor = { "색선택","검은색","파란색","빨간색","초록색","심홍색" }; 
 		cColor = new Choice();
-		cColor.add("색선택");
-		cColor.add("검은색");
-		cColor.add("파란색");
-		cColor.add("빨간색");
-		cColor.add("초록색");
-		cColor.add("심홍색");
+		for (int i=0; i<arrColor.length; i++) {
+			cColor.add(arrColor[i]);
+		}
 		cColor.setBounds(200, 80, 80, 20);
 		
 		setLayout(null);
