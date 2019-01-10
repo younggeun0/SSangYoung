09e4d46115d66df000fa36073f36093dec0cc14c
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-01-10 18:37:17 +0900

    190110 숙제
    FK 구현

---
+++ dev/workspace/sistJavaStudy2/src/date190110/hw/CreateTableDAO.java
@@ -0,0 +1,40 @@
+package date190110.hw;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class CreateTableDAO {
+	
+	public CreateTableDAO(String sql, CreateTableView ctv) throws SQLException {
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+		}
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+			String id = "scott";
+			String pw = "tiger";
+			
+			con = DriverManager.getConnection(url, id, pw);
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.execute();
+			JOptionPane.showMessageDialog(ctv, "테이블 생성 성공");
+			
+		} finally {
+			if (pstmt != null) { pstmt.close(); }
+			if (con != null) { con.close(); }
+		}
+	}
+}
---
+++ dev/workspace/sistJavaStudy2/src/date190110/hw/RunCreateTableView.java
@@ -0,0 +1,8 @@
+package date190110.hw;
+
+public class RunCreateTableView {
+
+	public static void main(String[] args) {
+		new CreateTableView();
+	}
+}
