fde873608d60ed91fdacafa92915d0006b95b30b
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-01-15 17:56:57 +0900

    190115 교육내용

--- dev/workspace/lunch_prj/src/kr/co/sist/lunch/admin/controller/LunchMainController.java
+++ dev/workspace/lunch_prj/src/kr/co/sist/lunch/admin/controller/LunchMainController.java
@@ -2,27 +2,103 @@ package kr.co.sist.lunch.admin.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Calendar;
 
+import javax.swing.JComboBox;
+
+import kr.co.sist.lunch.admin.model.LunchAdminDAO;
 import kr.co.sist.lunch.admin.view.LunchMainView;
 
-public class LunchMainController extends WindowAdapter implements ActionListener {
+public class LunchMainController extends WindowAdapter implements ActionListener,MouseListener {
 
 	private LunchMainView lmv;
+	private LunchAdminDAO la_dao;
 	
 	public LunchMainController(LunchMainView lmv) {
 		this.lmv =lmv;
+		la_dao = LunchAdminDAO.getInstance();
+	}
+	
+	private void setLunch() {
+
+	}
+	
+	private void searchOrder() {
+
+	}
+	
+	private void searchCalc() {
+
+	}
+	
+	/**
+	 * 월이 선택되면 해당년의 해당월의 마지막 날을 설정
+	 */
+	private void setDay() {
+		int selectedYear = ((Integer)lmv.getJcbYear().getSelectedItem()).intValue();
+		int selectedMonth = ((Integer)lmv.getJcbMonth().getSelectedItem()).intValue();
 		
+		// 마지막 날 얻기
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(Calendar.YEAR, selectedYear);
+		cal.set(Calendar.MONTH, selectedMonth-1); // 자바의 월은 사람이 생각하는 달보다 하나 작음
+
+		int lastDay = cal.getActualMaximum(Calendar.DATE);
+		int currDay = cal.get(Calendar.DAY_OF_MONTH);
+		
+		lmv.getCbmDay().removeAllElements(); // 모델을 초기화하고 선택된 날을 설정
+		for(int day = 1; day<lastDay+1; day++) {
+			lmv.getCbmDay().addElement(day);
+		}
+		lmv.getJcbDay().setSelectedItem(new Integer(currDay));
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent ae) {
+		if (ae.getSource() == lmv.getJcbMonth()) {
+			setDay();
+		}
 		
+		if (ae.getSource() == lmv.getJbtAddLunch()) {
+			
+		}
+		
+		if (ae.getSource() == lmv.getJbtCalcOrder()) {
+			
+		}
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent me) {
+		if (me.getSource() == lmv.getJtb()) {
+			
+		}
+		
+		if (me.getSource() == lmv.getJtLunch()) {
+			
+		}
+		
+		if (me.getSource() == lmv.getJtOrder()) {
+			
+		}
 	}
 	
 	@Override
 	public void windowClosing(WindowEvent e) {
 		lmv.dispose();
 	}
+	// 안쓰는 MouseListener methode들
+	@Override
+	public void mousePressed(MouseEvent e) {}
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+	@Override
+	public void mouseExited(MouseEvent e) {}
 }
--- dev/workspace/lunch_prj/src/kr/co/sist/lunch/admin/view/LunchMainView.java
+++ dev/workspace/lunch_prj/src/kr/co/sist/lunch/admin/view/LunchMainView.java
@@ -1,12 +1,18 @@
 package kr.co.sist.lunch.admin.view;
 
-import javax.swing.ComboBoxModel;
+import java.awt.BorderLayout;
+import java.util.Calendar;
+
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
 import kr.co.sist.lunch.admin.controller.LunchMainController;
@@ -20,24 +26,30 @@ public class LunchMainView extends JFrame {
 	private JComboBox<Integer> jcbYear, jcbMonth, jcbDay;
 	private DefaultComboBoxModel<Integer> cbmYear, cbmMonth, cbmDay;
 	private JTable jtLunch, jtOrder; // 메뉴, 주문
+	private Calendar cal;
 
 	public LunchMainView(String adminName) {
 		super("도시락 관리 [로그인 계정 : "+adminName+"]");
+		
+		cal = Calendar.getInstance();
 		
 		jtb = new JTabbedPane();
 		// 도시락 
 		String[] lunchColumns = { "번호", "도시락 코드", "이미지", "도시락명", "가격" };
 		dtmLunch = new DefaultTableModel(lunchColumns, 4);
+		jtLunch = new JTable(dtmLunch);
 		
 		// 주문
 		String[] orderColumns = { "번호", "주문번호","도시락 코드",
 				"도시락명", "주문자명", "수량", "가격", "주문일",
 				"연락처", "주문자 IP", "제작상태" };
 		dtmOrder = new DefaultTableModel(orderColumns, 4);
+		jtOrder = new JTable(dtmOrder);
 		
 		// 정산
 		String[] calcColumns = { "번호", "도시락명", "수량", "가격" };
 		dtmCalc = new DefaultTableModel(calcColumns, 4);
+		JTable jtCalc = new JTable(dtmCalc);
 		
 		jbtAddLunch = new JButton("도시락 추가");
 		jbtCalcOrder = new JButton("정산");
@@ -51,14 +63,141 @@ public class LunchMainView extends JFrame {
 		cbmDay = new DefaultComboBoxModel<Integer>();
 		jcbDay = new JComboBox<Integer>(cbmDay);
 		
+		JScrollPane jspLunch = new JScrollPane(jtLunch);
+		jspLunch.setBorder(new TitledBorder("도시락 목록"));
+		JScrollPane jspOrder = new JScrollPane(jtOrder);
+		jspOrder.setBorder(new TitledBorder("주문 목록"));
+		JScrollPane jspCalc = new JScrollPane(jtCalc);
+		jspCalc.setBorder(new TitledBorder("정산"));
 		
+		// 첫 탭(도시락)에 들어갈 컴포넌트 배치
+		JPanel jpLunch = new JPanel();
+		jpLunch.setLayout(new BorderLayout());
 		
+		JPanel jpLunchNorth = new JPanel();
+		jpLunchNorth.add(jbtAddLunch);
+		
+		jpLunch.add(BorderLayout.CENTER, jspLunch);
+		jpLunch.add(BorderLayout.NORTH, jpLunchNorth);
+		
+		jtb.add("도시락", jpLunch);
+
+		// 두번째 탭에 들어갈 컴포넌트 배치
+		JPanel jpOrder = new JPanel();
+		jpOrder.setLayout(new BorderLayout());
+		jpOrder.add(jspOrder);
+		
+		jtb.add("주문", jpOrder);
+		
+		// 두번째 탭에 들어갈 컴포넌트 배치
+		JPanel jpCalc = new JPanel();
+		jpCalc.setLayout(new BorderLayout());
+		
+		JPanel jpCalcNorth = new JPanel();
+		jpCalcNorth.setBorder(new TitledBorder("정산일자 선택"));
+		jpCalcNorth.add(jcbYear);
+		jpCalcNorth.add(new JLabel("년"));
+		jpCalcNorth.add(jcbMonth);
+		jpCalcNorth.add(new JLabel("월"));
+		jpCalcNorth.add(jcbDay);
+		jpCalcNorth.add(new JLabel("일"));
+		jpCalcNorth.add(jbtCalcOrder);
+		
+		jpCalc.add(BorderLayout.NORTH, jpCalcNorth);
+		jpCalc.add(BorderLayout.CENTER, jspCalc);
+		
+		jtb.add("주문", jpCalc);
+
+		add(jtb);
+		
+		setYear(); // JCB Year 설정
+		setMonth(); // JCB Month 설정
+		setDay(); // JCB Day 설정
 		
 		LunchMainController lmc = new LunchMainController(this);
+		
+		jbtAddLunch.addActionListener(lmc);
+		jbtCalcOrder.addActionListener(lmc);
+
+		jcbMonth.addActionListener(lmc); // 월을 변경하면 일이 바뀌도록 이벤트처리
+		
+		jtb.addMouseListener(lmc); // 탭에서 이벤트가 발생했을 때
+		jtLunch.addMouseListener(lmc);
+		jtOrder.addMouseListener(lmc);
 		
 		addWindowListener(lmc);
 		
 		setBounds(400, 200, 800, 600);
 		setVisible(true);
+	}
+	
+	public void setYear() { // 현재년도부터 4년전까지
+		int currYear = cal.get(Calendar.YEAR);
+		
+		for (int temp = 0; temp<4; temp++) {
+			cbmYear.addElement(currYear-temp);
+		}
+		jcbYear.setSelectedItem(new Integer(currYear));
+	}
+	
+	public void setMonth() { // 월 1~12월
+		int currMonth = cal.get(Calendar.DAY_OF_MONTH)+1;
+		for(int month=1; month<13; month++) {
+			cbmMonth.addElement(month);
+		}
+		jcbMonth.setSelectedItem(new Integer(currMonth));
+	}
+	
+	public void setDay() { // 그 월의 마지막 날
+		
+		int lastDay = cal.getActualMaximum(Calendar.DATE);
+		int currDay = cal.get(Calendar.DAY_OF_MONTH);
+		for(int day = 1; day<lastDay+1; day++) {
+			cbmDay.addElement(day);
+		}
+		jcbDay.setSelectedItem(new Integer(currDay));
+	}
+
+	public JTabbedPane getJtb() {
+		return jtb;
+	}
+	public DefaultTableModel getDtmLunch() {
+		return dtmLunch;
+	}
+	public DefaultTableModel getDtmOrder() {
+		return dtmOrder;
+	}
+	public DefaultTableModel getDtmCalc() {
+		return dtmCalc;
+	}
+	public JButton getJbtAddLunch() {
+		return jbtAddLunch;
+	}
+	public JButton getJbtCalcOrder() {
+		return jbtCalcOrder;
+	}
+	public JComboBox<Integer> getJcbYear() {
+		return jcbYear;
+	}
+	public JComboBox<Integer> getJcbMonth() {
+		return jcbMonth;
+	}
+	public JComboBox<Integer> getJcbDay() {
+		return jcbDay;
+	}
+	public DefaultComboBoxModel<Integer> getCbmYear() {
+		return cbmYear;
+	}
+	public DefaultComboBoxModel<Integer> getCbmMonth() {
+		return cbmMonth;
+	}
+	public DefaultComboBoxModel<Integer> getCbmDay() {
+		return cbmDay;
+	}
+	public JTable getJtLunch() {
+		return jtLunch;
+	}
+	public JTable getJtOrder() {
+		return jtOrder;
 	}
 }
